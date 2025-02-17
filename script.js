ff910bd4c4c903c0cf396640cbf5085c9f2b5ac7
Nusrat Jahan <nusratjahanitbd1@gmail.com>
2025-02-17 12:26:50 +0600

    Class-37

---
+++ script.js
@@ -0,0 +1,59 @@
+//1D array (index array)
+
+let marks = [49, 55, 60, 65, 70,]
+
+marks[2]
+console.log(marks[2]);
+
+let sum = marks[2]+marks[4];
+console.log(sum);
+
+//associative array (objective array)
+
+let man = {name: "Nusrat Jahan", age: "19", blood: "B+", phone: "+8801XXXXXXXX", address: "Dhaka", };
+
+//man.address;
+
+
+console.log(man.name);
+console.log(man.age);
+console.log(man.address);
+console.log(man.blood);
+console.log(man.phone);
+
+
+//2D Array (2 Dimensional Array)
+
+let twoDimensionalArray = [
+     [5, 50, 56, 46, 55],
+     [55, 66, 70, 75, 80],
+     [84, 90, 95, 100, 105],
+     [200, 300, 400, 500, 600],
+];
+
+console.log(twoDimensionalArray[0][0]);
+console.log(twoDimensionalArray[0][1]);
+console.log(twoDimensionalArray[0][2]);
+console.log(twoDimensionalArray[0][3]);
+console.log(twoDimensionalArray[0][4]);
+
+console.log(twoDimensionalArray[1][0]);
+console.log(twoDimensionalArray[1][1]);
+console.log(twoDimensionalArray[1][2]);
+console.log(twoDimensionalArray[1][3]);
+console.log(twoDimensionalArray[1][4]);
+
+console.log(twoDimensionalArray[2][0]);
+console.log(twoDimensionalArray[2][1]);
+console.log(twoDimensionalArray[2][2]);
+console.log(twoDimensionalArray[2][3]);
+console.log(twoDimensionalArray[2][4]);
+
+console.log(twoDimensionalArray[3][0]);
+console.log(twoDimensionalArray[3][1]);
+console.log(twoDimensionalArray[3][2]);
+console.log(twoDimensionalArray[3][3]);
+console.log(twoDimensionalArray[3][4]);
+
+
+
